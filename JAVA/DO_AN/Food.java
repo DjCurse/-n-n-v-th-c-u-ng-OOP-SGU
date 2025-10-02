32a7fb8397771b3b6dbf999e7564617a77d1de51
DjCurse <curseamountOVER2101@gmail.com>
2025-10-02 14:44:02 +0700

    Update Food.java
    Add more detail

--- JAVA/DO_AN/Food.java
+++ JAVA/DO_AN/Food.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
  class Food extends SanPham{
     private String DoChin; 
     private String LoaiThit; 
@@ -7,7 +10,7 @@
         LoaiThit = ""; 
     } 
 
-    public Food(String m, String t, int sl, char s, long d, int cal,
+    public Food(String m, String t, int sl, String s, long d, float cal,
     String dochin, String Lt ,String nsx, String HSD){
         super(m,t,s,d,sl,cal,nsx,HSD); 
         DoChin = dochin; 
@@ -18,7 +21,23 @@
         super((SanPham)f); 
         DoChin = f.DoChin; 
         LoaiThit = f.LoaiThit; 
+    }  
+
+    @Override
+    public void nhap(){ 
+        Scanner sc = new Scanner(System.in); 
+        super.nhap();
+        System.out.print("Nhap do chin: "); 
+        DoChin = sc.nextLine(); 
+        System.out.print("Nhap loai thit: "); 
+        LoaiThit = sc.nextLine(); 
     } 
+
+    @Override
+    public void xuat(){ 
+        super.xuat();
+        System.out.printf("| Do chin: %s | Loai thit: %s \n", DoChin, LoaiThit);
+    }
 
     public void setDoChin(String dc){
         DoChin = dc; 
