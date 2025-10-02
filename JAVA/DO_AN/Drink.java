a7ced27c88d8d3905dee15d8f73de68233fa0b02
DjCurse <curseamountOVER2101@gmail.com>
2025-10-02 14:43:32 +0700

    Update Drink.java
    So i add more detail to edit

--- JAVA/DO_AN/Drink.java
+++ JAVA/DO_AN/Drink.java
@@ -1,3 +1,61 @@
+
+import java.util.Scanner;
+
  class Drink extends SanPham{
-    private 
+    private float cafein; // dv: mg 
+    private float sugar; // dv: mg/dL 
+
+    public Drink(){ 
+        super();
+        cafein = 0; 
+        sugar = 0; 
+    } 
+
+    public Drink(String MaSP, String Ten, String Size, 
+                long DonGia, int SoLuong, float Calories, 
+                String NgaySanXuat, String HSD, float caf, float su){
+        super(MaSP, Ten, Size, DonGia, SoLuong, Calories, NgaySanXuat, HSD); 
+        this.cafein = caf; 
+        this.sugar = su; 
+    } 
+
+    public Drink(Drink dk) { 
+        super((SanPham)dk); 
+        this.cafein = dk.cafein; 
+        this.sugar = dk.sugar; 
+    } 
+    @Override
+    public void nhap(){ 
+        Scanner sc = new Scanner(System.in);
+        super.nhap();
+        System.out.println("Nhap luong cafein: ");  
+        cafein = sc.nextFloat(); 
+        System.out.println("Nhap luong duong: "); 
+        sugar = sc.nextFloat(); 
+    } 
+    @Override 
+    public  void xuat(){
+        super.xuat(); 
+        System.out.printf("| Luong cafein: %f | Luong duong: %f \n", cafein, sugar); 
+        
+    }  
+
+    public void setCafein(float cafe){
+        this.cafein = cafe; 
+    }  
+    
+    public void setSugar(float su){
+        this.sugar = su;
+    }
+
+    public float getCafein(){
+        return cafein; 
+    } 
+
+    public float getSugar(){
+        return sugar;
+    }
+
+
+
 }
