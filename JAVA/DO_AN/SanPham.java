f583c3ab4517b7fe7f1647ece4e8a051708c80b4
DjCurse <curseamountOVER2101@gmail.com>
2025-10-02 14:46:50 +0700

    Update SanPham.java
    add more detail

--- JAVA/DO_AN/SanPham.java
+++ JAVA/DO_AN/SanPham.java
@@ -1,30 +1,33 @@
+
+import java.util.Scanner;
+
 public class SanPham {
     private String MaSP; 
     private String Ten; 
-    private char Size;  
+    private String Size;  
     private long DonGia; 
-    private int SoLuong; 
-    private int Calories;  
+    private int SL; 
+    private float Calories;  
     private String NgaySanXuat; 
     private String HSD; 
 
     public SanPham() { 
         MaSP = ""; 
         Ten = ""; 
-        Size = ' '; 
+        Size = ""; 
         DonGia = 0; 
-        SoLuong = 0;    
+        SL = 0;    
         Calories = 0; 
         NgaySanXuat = ""; 
         HSD = ""; 
     } 
 
-    public SanPham(String MaSP, String Ten, char Size, long DonGia, int SoLuong, int Calories, String NgaySanXuat, String HSD) {
+    public SanPham(String MaSP, String Ten, String Size, long DonGia, int SL, float Calories, String NgaySanXuat, String HSD) {
         this.MaSP = MaSP;
         this.Ten = Ten;
         this.Size = Size;
         this.DonGia = DonGia;
-        this.SoLuong = SoLuong;
+        this.SL = SL;
         this.Calories = Calories;
         this.NgaySanXuat = NgaySanXuat;
         this.HSD = HSD;
@@ -35,10 +38,43 @@ public class SanPham {
         this.Ten = fd.Ten;
         this.Size = fd.Size;
         this.DonGia = fd.DonGia;
-        this.SoLuong = fd.SoLuong;
+        this.SL = fd.SL;
         this.Calories = fd.Calories;
         this.NgaySanXuat = fd.NgaySanXuat;
         this.HSD = fd.HSD;
+    }  
+
+    public void nhap(){ 
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Nhap ma san pham: ");  
+        MaSP = sc.nextLine(); 
+        System.out.print("Nhap ten san pham: ");  
+        Ten = sc.nextLine(); 
+        System.out.print("Nhap size: ");  
+        Size = sc.nextLine();
+        System.out.print("Nhap don gia: ");  
+        DonGia = sc.nextLong(); 
+        System.out.print("Nhap so luong: ");  
+        SL = sc.nextInt(); 
+        System.out.print("nhap luong Calories: ");  
+        Calories = sc.nextFloat(); 
+
+        System.out.print("Nhap ngay : ");  
+        int ngay = sc.nextInt(); 
+
+        System.out.print("Nhap thang : ");   
+        int thang = sc.nextInt(); 
+
+        System.out.print("Nhap nam : ");  
+        int nam = sc.nextInt();   
+        
+        sc.nextLine(); 
+        HSD = String.format("%02d/%02d/%04d", ngay, thang, nam);
+    } 
+
+    public void xuat(){
+        System.out.printf("\n | Ma san pham: %s | Ten san pham: %s | Size: %s | don gia: %l | Calories: %f | HSD: %s "
+                            , MaSP, Ten, Size, DonGia, SL, Calories, HSD); 
     } 
 
     public String getMaSP() {
@@ -53,10 +89,10 @@ public class SanPham {
     public void setTen(String Ten) {
         this.Ten = Ten;
     }   
-    public char getSize() {
+    public String getSize() {
         return Size;
     } 
-    public void setSize(char Size) {
+    public void setSize(String Size) {
         this.Size = Size;
     } 
     public long getDonGia() {
@@ -65,13 +101,13 @@ public class SanPham {
     public void setDonGia(long DonGia) {
         this.DonGia = DonGia;
     } 
-    public int getSoLuong() {
-        return SoLuong;
+    public int getSL() {
+        return SL;
     } 
-    public void setSoLuong(int SoLuong) {
-        this.SoLuong = SoLuong;
+    public void setSL(int SL) {
+        this.SL = SL;
     } 
-    public int getCalories() {
+    public float getCalories() {
         return Calories;
     } 
     public void setCalories(int Calories) {
